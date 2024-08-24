d25dcb9f7f0fa2b304204cdc4412077fb2f3573e
pascal-ik <18ik052@esisalama.org>
2024-08-24 20:53:07 +0200

    adding project

---
+++ skeleton/src/Entity/Equipe.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EquipeRepository::class)]
+class Equipe
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $nombre_membre = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getNombreMembre(): ?int
+    {
+        return $this->nombre_membre;
+    }
+
+    public function setNombreMembre(int $nombre_membre): static
+    {
+        $this->nombre_membre = $nombre_membre;
+
+        return $this;
+    }
+}
---
+++ skeleton/src/Entity/Projet.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjetRepository::class)]
+class Projet
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $start_at = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $end_at = null;
+
+    #[ORM\Column]
+    private ?int $cout = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeInterface
+    {
+        return $this->start_at;
+    }
+
+    public function setStartAt(\DateTimeInterface $start_at): static
+    {
+        $this->start_at = $start_at;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeInterface
+    {
+        return $this->end_at;
+    }
+
+    public function setEndAt(\DateTimeInterface $end_at): static
+    {
+        $this->end_at = $end_at;
+
+        return $this;
+    }
+
+    public function getCout(): ?int
+    {
+        return $this->cout;
+    }
+
+    public function setCout(int $cout): static
+    {
+        $this->cout = $cout;
+
+        return $this;
+    }
+}
